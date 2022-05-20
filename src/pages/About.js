a88149653d97ecb6855fa4fed267d1d79c983ce1
Magydream <87599508+Magydream@users.noreply.github.com>
2022-05-20 16:40:37 +0200

    reactprojet

---
+++ src/pages/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Navigation from './../components/Navigation';
+import Logo from './../components/Logo';
+
+const About = () => {
+  return (
+    <div>
+      <Logo/>
+        <Navigation/>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex
+        autem, est iste deleniti aliquam facilis exercitationem totam? Nihil
+        atque voluptate reprehenderit! Minima ab non, quia a repellendus eum,
+        ipsam asperiores et qui laborum quam molestias numquam deserunt, dicta
+        nemo ut eligendi consequuntur! Nisi fuga et, nemo pariatur iure,
+        deserunt vel enim libero reiciendis est rem velit, corrupti dolorum. Non
+        sapiente sunt voluptates hic nemo minima eligendi quae sit asperiores!
+        Laboriosam consectetur itaque delectus temporibus vitae quia quibusdam
+        voluptatum ut laborum omnis sit exercitationem aut dicta sed voluptas,
+        hic odit? Laborum, asperiores tenetur pariatur molestias voluptatibus
+        neque minus fuga corporis accusamus vitae facilis ut consequatur vel
+        culpa laudantium voluptatem! Molestiae non excepturi optio deserunt
+        velit vero facere sequi quibusdam assumenda voluptatum, maiores iste
+        vitae harum quos placeat id, repudiandae ipsam, officia hic? Ad ipsa
+        minus sint veritatis iste quisquam, corporis sit nam tempora error
+        necessitatibus a in deleniti enim quibusdam tempore, praesentium
+        recusandae velit voluptatem libero accusamus! Asperiores laudantium,
+        dolorum accusantium harum perspiciatis maiores accusamus eum fuga
+        dignissimos a tenetur? Aspernatur, deleniti exercitationem non iure
+        iusto labore atque? Non quis blanditiis cum, error ad at nisi delectus
+        quos corporis unde dolorum aspernatur dolorem, minus minima quisquam
+        fugit, sed nesciunt natus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
